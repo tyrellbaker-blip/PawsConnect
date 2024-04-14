b20f18534bdef592eb7dfc1a41cbb06c6afcde28
tyrellbaker-blip <91085471+tyrellbaker-blip@users.noreply.github.com>
2024-04-14 03:14:06 -0700

    Saving API endpoints

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,26 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="registerContent">
+    <h2 class="text-black">Register</h2>
+    <RegistrationForm />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import RegistrationForm from './components/RegistrationForm.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    RegistrationForm
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+/* Styles will be added later */
 </style>
